c8de18f6ede1a4d733e19f949eef355be5fe0c0b
blmccavanagh <blmccavanagh@gmail.com>
2021-06-01 16:56:16 +0800

    index.js moved to root folder.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const connection = require('../config/connection');
+const connection = require('./config/connection');
 const inquirer = require('inquirer');
 // can install npm i console.table at a later date
 // require('console.table');
@@ -73,7 +73,7 @@ function accessDb() {
                 connection.end();
                 break;
         }
-    })
+    });
 };
 
 function viewDepartment() {
